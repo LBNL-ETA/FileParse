e39a453ed4639287c25dbde10596276fb6988849
Simon Vidanovic <dvvidanovic@lbl.gov>
2023-10-04 13:48:21 -0700

    Variant serialize/deserialize functions added.

--- test/helper/TestSerializers.cxx
+++ test/helper/TestSerializers.cxx
@@ -2,24 +2,28 @@
 
 XMLNodeAdapter operator>>(const XMLNodeAdapter & xmlNode, Helper::BaseElement & base)
 {
-    xmlNode >> Child("SampleText", base.text);
-    xmlNode >> Child("Integer", base.integer_number);
-    xmlNode >> Child("Double", base.double_number);
-    xmlNode >> Child("OptionalText", base.optional_text);
-    xmlNode >> Child("OptionalInteger", base.optional_int);
-    xmlNode >> Child("OptionalDouble", base.optional_double);
+    xmlNode >> Child{"SampleText", base.text};
+    xmlNode >> Child{"Integer", base.integer_number};
+    xmlNode >> Child{"Double", base.double_number};
+    xmlNode >> Child{"OptionalText", base.optional_text};
+    xmlNode >> Child{"OptionalInteger", base.optional_int};
+    xmlNode >> Child{"OptionalDouble", base.optional_double};
+    deserializeVariant(xmlNode, {"VariantDouble", "VariantString"}, base.variant_field);
+    //xmlNode >> Child{{"Double", "String"}, base.variant_field};
 
     return xmlNode;
 }
 
 XMLNodeAdapter operator<<(XMLNodeAdapter xmlNode, const Helper::BaseElement & base)
 {
-    xmlNode << Child("SampleText", base.text);
-    xmlNode << Child("Integer", base.integer_number);
-    xmlNode << Child("Double", base.double_number);
-    xmlNode << Child("OptionalText", base.optional_text);
-    xmlNode << Child("OptionalInteger", base.optional_int);
-    xmlNode << Child("OptionalDouble", base.optional_int);
+    xmlNode << Child{"SampleText", base.text};
+    xmlNode << Child{"Integer", base.integer_number};
+    xmlNode << Child{"Double", base.double_number};
+    xmlNode << Child{"OptionalText", base.optional_text};
+    xmlNode << Child{"OptionalInteger", base.optional_int};
+    xmlNode << Child{"OptionalDouble", base.optional_int};
+    serializeVariant(xmlNode, {"VariantDouble", "VariantString"}, base.variant_field);
+    //xmlNode << Child{{"Double", "String"}, base.variant_field};
 
     return xmlNode;
 }
--- test/helper/TestStructures.hxx
+++ test/helper/TestStructures.hxx
@@ -2,6 +2,7 @@
 
 #include <string>
 #include <optional>
+#include <variant>
 
 namespace Helper
 {
@@ -13,5 +14,6 @@ namespace Helper
         std::optional<std::string> optional_text;
         std::optional<int> optional_int;
         std::optional<double> optional_double;
+        std::variant<double, std::string> variant_field{0.0};
     };
 }   // namespace Helper
