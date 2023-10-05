6b70f26747bcf378229a57c7d5eb01c1943ea1ab
Simon Vidanovic <dvvidanovic@lbl.gov>
2023-10-05 09:49:37 -0700

    Comparing two vectors function will now report exact place on where the test fails.

--- test/helper/Utility.hxx
+++ test/helper/Utility.hxx
@@ -9,11 +9,13 @@ namespace Helper
     template<typename T>
     void checkVectorValues(const std::vector<T> & correct, const std::vector<T> & values, T tolerance)
     {
-        ASSERT_EQ(correct.size(), values.size());
+        ASSERT_EQ(correct.size(), values.size()) << "Vectors have different sizes: expected " << correct.size() << ", but got " << values.size();
 
         for(size_t i = 0u; i < correct.size(); ++i)
         {
-            EXPECT_NEAR(correct[i], values[i], tolerance);
+            std::ostringstream message;
+            message << "At index " << i << ": expected " << correct[i] << ", but got " << values[i] << " with tolerance " << tolerance;
+            EXPECT_NEAR(correct[i], values[i], tolerance) << message.str();
         }
     }
-}   // namespace Helper
+}
