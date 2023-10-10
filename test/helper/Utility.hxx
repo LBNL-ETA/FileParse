4fdb3722d7aff81abdab54d09a5cce8f35264984
Simon Vidanovic <dvvidanovic@lbl.gov>
2023-10-10 13:33:28 -0700

    Set reading added.

--- test/helper/Utility.hxx
+++ test/helper/Utility.hxx
@@ -20,4 +20,23 @@ namespace Helper
             EXPECT_NEAR(correct[i], values[i], tolerance) << message.str();
         }
     }
+
+    template<typename T>
+    void checkSetValues(const std::set<T> & correct, const std::set<T> & values, T tolerance)
+    {
+        ASSERT_EQ(correct.size(), values.size())
+          << "Sets have different sizes: expected " << correct.size() << ", but got " << values.size();
+
+        auto correctIt = correct.begin();
+        auto valuesIt = values.begin();
+
+        for(size_t i = 0u; correctIt != correct.end() && valuesIt != values.end(); ++correctIt, ++valuesIt, ++i)
+        {
+            std::ostringstream message;
+            message << "At position " << i << ": expected " << *correctIt << ", but got " << *valuesIt
+                    << " with tolerance " << tolerance;
+            EXPECT_NEAR(*correctIt, *valuesIt, tolerance) << message.str();
+        }
+    }
+
 }   // namespace Helper
