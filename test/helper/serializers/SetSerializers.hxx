10112a55fe5318c750ed025a726cf1746230ed64
Simon Vidanovic <dvvidanovic@lbl.gov>
2023-10-11 11:07:14 -0700

    Optional set reading/writing implemented.

--- test/helper/serializers/SetSerializers.hxx
+++ test/helper/serializers/SetSerializers.hxx
@@ -8,6 +8,12 @@ namespace Helper
     inline XMLNodeAdapter operator>>(const XMLNodeAdapter & xmlNode, Helper::SetElementDouble & element);
     inline XMLNodeAdapter operator<<(XMLNodeAdapter xmlNode, const Helper::SetElementDouble & element);
 
+    inline XMLNodeAdapter operator>>(const XMLNodeAdapter & xmlNode, Helper::SetElementOptionalDouble & element);
+    inline XMLNodeAdapter operator<<(XMLNodeAdapter xmlNode, const Helper::SetElementOptionalDouble & element);
+
     SetElementDouble loadSetElementDouble(std::string_view fileName);
     void saveSetElementDouble(const SetElementDouble& element, std::string_view fileName);
+
+    SetElementOptionalDouble loadSetElementOptionalDouble(std::string_view fileName);
+    void saveSetElementOptionalDouble(const SetElementOptionalDouble& element, std::string_view fileName);
 }
