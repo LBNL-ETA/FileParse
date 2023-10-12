735d1e6dd494c9b97858340d95cf757741c5a639
Simon Vidanovic <dvvidanovic@lbl.gov>
2023-10-12 15:44:52 -0700

    Templates are now adopted for the map and unordered_map

--- src/Map.hxx
+++ src/Map.hxx
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <map>
 #include <functional>
 
 #include "StringConversion.hxx"
@@ -8,8 +7,9 @@
 
 namespace FileParse
 {
-    template<typename NodeAdapter, typename Value>
-    inline NodeAdapter operator<<(NodeAdapter node, const Child<const std::map<std::string, Value>> & value)
+    template<typename NodeAdapter, typename MapType>
+    inline std::enable_if_t<is_valid_map<MapType>::value, NodeAdapter> operator<<(NodeAdapter node,
+                                                                                  const Child<const MapType> & value)
     {
         if(value.nodeNames.empty() || value.data.empty())
             return node;
@@ -21,8 +21,9 @@ namespace FileParse
         return node;
     }
 
-    template<typename NodeAdapter, typename Value>
-    inline NodeAdapter operator>>(NodeAdapter node, Child<const std::map<std::string, Value>> & value)
+    template<typename NodeAdapter, typename MapType>
+    inline std::enable_if_t<is_valid_map<MapType>::value, NodeAdapter> operator>>(NodeAdapter node,
+                                                                                  Child<const MapType> & value)
     {
         if(value.nodeNames.empty())
         {
@@ -44,15 +45,18 @@ namespace FileParse
         return node;
     }
 
-    template<typename NodeAdapter, typename EnumType, typename Value>
-    NodeAdapter serializeEnumMap(NodeAdapter node,
-                                 const std::map<EnumType, Value> & map,
-                                 std::function<std::string(EnumType)> converter)
+    template<typename NodeAdapter, typename EnumType, typename ValueType>
+    inline std::enable_if_t<is_valid_map<std::map<EnumType, ValueType>>::value
+                              || is_valid_map<std::unordered_map<EnumType, ValueType>>::value,
+                            NodeAdapter>
+      serializeEnumMap(NodeAdapter node,
+                       const std::map<EnumType, ValueType> & map,
+                       std::function<std::string(EnumType)> converter)
     {
         if(map.empty())
             return node;
 
-        std::map<std::string, Value> mapToSerialize;
+        std::map<std::string, ValueType> mapToSerialize;
         for(const auto & [enumKey, mapValue] : map)
         {
             mapToSerialize[converter(enumKey)] = mapValue;
@@ -63,10 +67,13 @@ namespace FileParse
         return node;
     }
 
-    template<typename NodeAdapter, typename EnumType, typename Value>
-    NodeAdapter deserializeEnumMap(const NodeAdapter & node,
-                                   std::map<EnumType, Value> & map,
-                                   std::function<EnumType(std::string_view)> converter)
+    template<typename NodeAdapter, typename EnumType, typename ValueType>
+    inline std::enable_if_t<is_valid_map<std::map<EnumType, ValueType>>::value
+                              || is_valid_map<std::unordered_map<EnumType, ValueType>>::value,
+                            NodeAdapter>
+      deserializeEnumMap(const NodeAdapter & node,
+                         std::map<EnumType, ValueType> & map,
+                         std::function<EnumType(std::string_view)> converter)
     {
         int totalNodes = node.nChildNode();
         for(int i = 0; i < totalNodes; ++i)
@@ -76,7 +83,7 @@ namespace FileParse
             {
                 const auto text = childNode.getText();
                 const auto key = childNode.getCurrentTag();
-                map[converter(key)] = from_string_helper<Value>(text);
+                map[converter(key)] = from_string_helper<ValueType>(text);
             }
         }
 
