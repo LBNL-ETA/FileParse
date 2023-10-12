0d96c7722129fc62ed7a595e08db4ae38292b210
Simon Vidanovic <dvvidanovic@lbl.gov>
2023-10-12 08:30:17 -0700

    Optional string map read/write added.

--- test/helper/files/MapElementXML.hxx
+++ test/helper/files/MapElementXML.hxx
@@ -6,4 +6,7 @@ namespace Helper
 {
     std::string testMapElementStringDatabase();
     std::string mapElementString();
+
+    std::string testMapElementOptionalStringDatabase();
+    std::string mapElementOptionalString();
 }
--- test/helper/structures/StructureMap.hxx
+++ test/helper/structures/StructureMap.hxx
@@ -2,11 +2,17 @@
 
 #include <string>
 #include <map>
+#include <optional>
 
 namespace Helper
 {
     struct MapElementString
     {
         std::map<std::string, std::string> values;
+    };
+
+    struct MapElementOptionalString
+    {
+        std::optional<std::map<std::string, std::string>> values;
     };
 }   // namespace Helper
