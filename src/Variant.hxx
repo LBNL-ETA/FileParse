72ba4c3674d529f3d76b23e6a3636cdfa6beb0a2
vidanovic <vidanovicd@gmail.com>
2023-10-12 19:36:46 -0700

    Some minor code cleanups (from static analysis)

--- src/Variant.hxx
+++ src/Variant.hxx
@@ -4,34 +4,30 @@
 
 #include "Common.hxx"
 
-namespace FileParse
-{
+namespace FileParse {
     template<typename NodeAdapter, typename... Ts>
-    void serializeVariant(NodeAdapter & node,
-                          const std::vector<std::string> & nodeNames,
-                          const std::variant<Ts...> & variantValue)
-    {
-        if(variantValue.index() < nodeNames.size())
-        {
-            std::visit([&](const auto & val) { node << Child{nodeNames[variantValue.index()], val}; }, variantValue);
+    void serializeVariant(NodeAdapter &node,
+                          const std::vector<std::string> &nodeNames,
+                          const std::variant<Ts...> &variantValue) {
+        if (variantValue.index() < nodeNames.size()) {
+            std::visit([&node, &nodeNames, &variantValue](const auto &val) {
+                node << Child{nodeNames[variantValue.index()], val};
+            }, variantValue);
         }
     }
 
     template<typename NodeAdapter, typename... Ts>
-    void deserializeVariant(const NodeAdapter & node,
-                            const std::vector<std::string> & nodeNames,
-                            std::variant<Ts...> & variantValue)
-    {
+    void deserializeVariant(const NodeAdapter &node,
+                            const std::vector<std::string> &nodeNames,
+                            std::variant<Ts...> &variantValue) {
         bool deserialized = false;
         int index = 0;
 
-        auto try_deserialize_variant = [&](auto && dummyType) {
-            using Type = std::decay_t<decltype(dummyType)>;
-            if(!deserialized && index < nodeNames.size())
-            {
+        auto try_deserialize_variant = [&](auto dummyType) {
+            using Type = decltype(dummyType);
+            if (!deserialized && index < nodeNames.size()) {
                 Type value;
-                if(node.nChildNode(nodeNames[index]) > 0)
-                {
+                if (node.nChildNode(nodeNames[index]) > 0) {
                     node >> Child{nodeNames[index], value};
                     variantValue = value;
                     deserialized = true;
