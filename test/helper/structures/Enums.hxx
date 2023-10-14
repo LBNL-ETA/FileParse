742f4453346f70df6e47f6bf89b49fbd7504785b
vidanovic <vidanovicd@gmail.com>
2023-10-14 10:42:38 -0700

    Clang-format update and some intializations fixes.

--- test/helper/structures/Enums.hxx
+++ test/helper/structures/Enums.hxx
@@ -4,8 +4,10 @@
 #include <string>
 #include <algorithm>
 
-namespace Helper {
-    enum class Day {
+namespace Helper
+{
+    enum class Day
+    {
         None,
         Monday,
         Tuesday,
@@ -16,49 +18,42 @@ namespace Helper {
         Sunday
     };
 
-    constexpr std::array<std::string_view, 8> DayToString = {
-            "None",
-            "Monday",
-            "Tuesday",
-            "Wednesday",
-            "Thursday",
-            "Friday",
-            "Saturday",
-            "Sunday"
-    };
+    constexpr std::array<std::string_view, 8> DayToString
+      = {"None", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
 
-    inline std::string toDayString(Day day) {
+    inline std::string toDayString(Day day)
+    {
         return std::string{DayToString[static_cast<int>(day)]};
     }
 
-    inline Day toDay(std::string_view str) {
-        auto it = std::find(DayToString.begin(), DayToString.end(), str);
-        if (it != DayToString.end())
+    inline Day toDay(std::string_view str)
+    {
+        if(auto it = std::find(DayToString.begin(), DayToString.end(), str);
+           it != DayToString.end())
             return static_cast<Day>(std::distance(DayToString.begin(), it));
         return Day::None;
     }
 
-    enum class Color {
+    enum class Color
+    {
         None,
         Red,
         Green,
         Blue
     };
 
-    constexpr std::array<std::string_view, 4> ColorToString = {
-            "None",
-            "Red",
-            "Green",
-            "Blue"
-    };
+    constexpr std::array<std::string_view, 4> ColorToString = {"None", "Red", "Green", "Blue"};
 
-    inline std::string toColorString(Color color) {
+    inline std::string toColorString(Color color)
+    {
         return std::string{ColorToString[static_cast<int>(color)]};
     }
 
-    inline Color toColor(std::string_view str) {
-        if (auto it = std::find(ColorToString.begin(), ColorToString.end(), str);it != ColorToString.end())
+    inline Color toColor(std::string_view str)
+    {
+        if(auto it = std::find(ColorToString.begin(), ColorToString.end(), str);
+           it != ColorToString.end())
             return static_cast<Color>(std::distance(ColorToString.begin(), it));
         return Color::None;
     }
-}
+}   // namespace Helper
