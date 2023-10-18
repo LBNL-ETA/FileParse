9a932163de6550e11f0590359e8abfb7030ba8a3
Simon Vidanovic <dvvidanovic@lbl.gov>
2023-10-18 10:22:05 -0700

    All test serializers are templated.

--- test/helper/serializers/SerializersSet.hxx
+++ test/helper/serializers/SerializersSet.hxx
@@ -1,25 +1,84 @@
 #pragma once
 
+#include "Common.hxx"
+#include "Set.hxx"
+
 #include "test/helper/structures/StructureSet.hxx"
-#include "XMLNodeAdapter.hxx"
 
 namespace Helper
 {
-    inline XMLNodeAdapter operator>>(const XMLNodeAdapter & xmlNode, Helper::SetElementDouble & element);
-    inline XMLNodeAdapter operator<<(XMLNodeAdapter xmlNode, const Helper::SetElementDouble & element);
+    template<typename NodeAdapter>
+    inline NodeAdapter operator>>(const NodeAdapter & node, Helper::SetElementDouble & element)
+    {
+        using FileParse::Child;
+        using FileParse::operator>>;
 
-    inline XMLNodeAdapter operator>>(const XMLNodeAdapter & xmlNode, Helper::SetElementOptionalDouble & element);
-    inline XMLNodeAdapter operator<<(XMLNodeAdapter xmlNode, const Helper::SetElementOptionalDouble & element);
+        node >> Child{{"Table", "Value"}, element.values};
 
-    inline XMLNodeAdapter operator>>(const XMLNodeAdapter & xmlNode, Helper::SetElementEnum & element);
-    inline XMLNodeAdapter operator<<(XMLNodeAdapter xmlNode, const Helper::SetElementEnum & element);
+        return node;
+    }
+
+    template<typename NodeAdapter>
+    inline NodeAdapter operator<<(NodeAdapter node, const Helper::SetElementDouble & element)
+    {
+        using FileParse::Child;
+        using FileParse::operator<<;
+
+        node << Child{{"Table", "Value"}, element.values};
+
+        return node;
+    }
+
+    template<typename NodeAdapter>
+    inline NodeAdapter operator>>(const NodeAdapter & node,
+                                  Helper::SetElementOptionalDouble & element)
+    {
+        using FileParse::Child;
+        using FileParse::operator>>;
+
+        node >> Child{{"Table", "Value"}, element.values};
+
+        return node;
+    }
+
+    template<typename NodeAdapter>
+    inline NodeAdapter operator<<(NodeAdapter node,
+                                  const Helper::SetElementOptionalDouble & element)
+    {
+        using FileParse::Child;
+        using FileParse::operator<<;
+
+        node << Child{{"Table", "Value"}, element.values};
+
+        return node;
+    }
+
+    template<typename NodeAdapter>
+    inline NodeAdapter operator>>(const NodeAdapter & node, Helper::SetElementEnum & element)
+    {
+        FileParse::deserializeEnumSet<NodeAdapter, Helper::Day>(
+          node, {"Table", "Value"}, element.days, toDay);
+
+        return node;
+    }
+
+    template<typename NodeAdapter>
+    inline NodeAdapter operator<<(NodeAdapter node,
+                                     const Helper::SetElementEnum & element)
+    {
+        FileParse::serializeEnumSet<NodeAdapter, Helper::Day>(
+          node, {"Table", "Value"}, element.days, toDayString);
+
+        return node;
+    }
 
     SetElementDouble loadSetElementDouble(std::string_view fileName);
-    void saveSetElementDouble(const SetElementDouble& element, std::string_view fileName);
+    void saveSetElementDouble(const SetElementDouble & element, std::string_view fileName);
 
     SetElementOptionalDouble loadSetElementOptionalDouble(std::string_view fileName);
-    void saveSetElementOptionalDouble(const SetElementOptionalDouble& element, std::string_view fileName);
+    void saveSetElementOptionalDouble(const SetElementOptionalDouble & element,
+                                      std::string_view fileName);
 
     SetElementEnum loadSetElementEnum(std::string_view fileName);
-    void saveSetElementEnum(const SetElementEnum& element, std::string_view fileName);
-}
+    void saveSetElementEnum(const SetElementEnum & element, std::string_view fileName);
+}   // namespace Helper
