b9bb4d46f585a57c9b162f78be0638f46c766c85
vidanovic <dvvidanovic@lbl.gov>
2024-01-26 19:04:34 -0800

    Enum functions are changed to return and accept references.

--- src/FP_Enum.hxx
+++ src/FP_Enum.hxx
@@ -4,12 +4,14 @@
 #include <functional>
 #include <optional>
 
-namespace FileParse {
+namespace FileParse
+{
     template<typename NodeAdapter, typename EnumType>
-    NodeAdapter serializeEnum(NodeAdapter node,
-                              const std::string &tag,
-                              const EnumType &e,
-                              std::function<std::string(EnumType)> converter) {
+    NodeAdapter & serializeEnum(NodeAdapter & node,
+                                const std::string & tag,
+                                const EnumType & e,
+                                std::function<std::string(EnumType)> converter)
+    {
         static_assert(std::is_enum_v<EnumType>, "Provided type is not an enum!");
 
         NodeAdapter childNode = node.addChild(tag);
@@ -20,14 +22,16 @@ namespace FileParse {
     }
 
     template<typename NodeAdapter, typename EnumType>
-    NodeAdapter deserializeEnum(const NodeAdapter &node,
-                                const std::string &tag,
-                                EnumType &e,
-                                std::function<EnumType(std::string_view)> converter) {
+    const NodeAdapter & deserializeEnum(const NodeAdapter & node,
+                                        const std::string & tag,
+                                        EnumType & e,
+                                        std::function<EnumType(std::string_view)> converter)
+    {
         static_assert(std::is_enum_v<EnumType>, "Provided type is not an enum!");
 
 
-        if (auto childNode{node.getChildNode(tag, 0)}; !childNode.isEmpty()) {
+        if(auto childNode{node.getChildNode(tag, 0)}; !childNode.isEmpty())
+        {
             const auto text = childNode.getText();
             e = converter(text);
         }
@@ -36,28 +40,31 @@ namespace FileParse {
     }
 
     template<typename NodeAdapter, typename EnumType>
-    NodeAdapter serializeEnum(NodeAdapter node,
-                              const std::string &tag,
-                              const std::optional<EnumType> &opt,
-                              std::function<std::string(EnumType)> converter) {
-        if (opt.has_value())
+    NodeAdapter & serializeEnum(NodeAdapter & node,
+                                const std::string & tag,
+                                const std::optional<EnumType> & opt,
+                                std::function<std::string(EnumType)> converter)
+    {
+        if(opt.has_value())
             serializeEnum(node, tag, opt.value(), converter);
 
         return node;
     }
 
     template<typename NodeAdapter, typename EnumType>
-    NodeAdapter deserializeEnum(const NodeAdapter &node,
-                                const std::string &tag,
-                                std::optional<EnumType> &e_opt,
-                                std::function<EnumType(std::string_view)> converter) {
+    const NodeAdapter & deserializeEnum(const NodeAdapter & node,
+                                        const std::string & tag,
+                                        std::optional<EnumType> & e_opt,
+                                        std::function<EnumType(std::string_view)> converter)
+    {
         static_assert(std::is_enum_v<EnumType>, "Provided type is not an enum!");
 
-        if (auto childNode = node.getChildNode(tag, 0); !childNode.isEmpty()) {
+        if(auto childNode = node.getChildNode(tag, 0); !childNode.isEmpty())
+        {
             const auto text = childNode.getText();
             e_opt = converter(text);
         }
 
         return node;
     }
-}
+}   // namespace FileParse
