e21b85aab5fcb1ec55988c7bf72280d33802dfeb
Simon Vidanovic <dvvidanovic@lbl.gov>
2024-09-20 13:24:10 -0700

    Attributes example is now using generalized template functions (load and save) to load test example. Test in VS and it is working as well.

--- test/units/AttributesSerializers.unit.cxx
+++ test/units/AttributesSerializers.unit.cxx
@@ -3,25 +3,25 @@
 
 #include "test/helper/structures/Attributes.hxx"
 #include "test/helper/files/AttributesXML.hxx"
-#include "test/helper/FileManipulation.hxx"
 
 TEST(AttributesSerializerTest, Deserialize)
 {
     std::filesystem::path productPath{TEST_DATA_DIR};
     const auto fileName = productPath / "Attributes.xml";
 
-    Helper::AttributesTest base{Helper::loadAttributesElement(fileName.string())};
+    const auto base{Helper::loadAttributesElement(fileName.string())};
+    ASSERT_TRUE(base.has_value());
 
-    EXPECT_EQ("Text", base.name);
-    EXPECT_EQ(23, base.age);
-    EXPECT_DOUBLE_EQ(1.93, base.height);
-    EXPECT_EQ(Helper::Day::Tuesday, base.day);
-    ASSERT_TRUE(base.optional_age.has_value());
-    EXPECT_EQ(18, base.optional_age.value());
-    ASSERT_TRUE(base.nickname.has_value());
-    EXPECT_EQ("Charlie", base.nickname.value());
-    ASSERT_TRUE(base.color.has_value());
-    EXPECT_EQ(Helper::Color::Green, base.color.value());
+    EXPECT_EQ("Text", base->name);
+    EXPECT_EQ(23, base->age);
+    EXPECT_DOUBLE_EQ(1.93, base->height);
+    EXPECT_EQ(Helper::Day::Tuesday, base->day);
+    ASSERT_TRUE(base->optional_age.has_value());
+    EXPECT_EQ(18, base->optional_age.value());
+    ASSERT_TRUE(base->nickname.has_value());
+    EXPECT_EQ("Charlie", base->nickname.value());
+    ASSERT_TRUE(base->color.has_value());
+    EXPECT_EQ(Helper::Color::Green, base->color.value());
 }
 
 TEST(AttributesSerializerTest, Serialize)
@@ -35,15 +35,16 @@ TEST(AttributesSerializerTest, Serialize)
 
     EXPECT_EQ(0, result);
 
-    Helper::AttributesTest loadedBase{Helper::loadAttributesElement(fileName)};
+    const auto loadedBase{Helper::loadAttributesElement(fileName)};
+    ASSERT_TRUE(loadedBase.has_value());
 
-    EXPECT_EQ(base.name, loadedBase.name);
-    EXPECT_EQ(base.age, loadedBase.age);
-    EXPECT_DOUBLE_EQ(base.height, loadedBase.height);
-    EXPECT_EQ(base.day, loadedBase.day);
-    EXPECT_FALSE(loadedBase.optional_age.has_value());
-    EXPECT_EQ(base.nickname, loadedBase.nickname);
-    EXPECT_EQ(base.color, loadedBase.color);
+    EXPECT_EQ(base.name, loadedBase->name);
+    EXPECT_EQ(base.age, loadedBase->age);
+    EXPECT_DOUBLE_EQ(base.height, loadedBase->height);
+    EXPECT_EQ(base.day, loadedBase->day);
+    EXPECT_FALSE(loadedBase->optional_age.has_value());
+    EXPECT_EQ(base.nickname, loadedBase->nickname);
+    EXPECT_EQ(base.color, loadedBase->color);
 
     std::remove(fileName.c_str());
 }
