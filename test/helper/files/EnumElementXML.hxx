4cf2603cf6c70d958ff590f7e10358702c1df4ca
Simon Vidanovic <dvvidanovic@lbl.gov>
2024-09-20 14:03:58 -0700

    EnumElement is now using general function and test file.

--- test/helper/files/EnumElementXML.hxx
+++ test/helper/files/EnumElementXML.hxx
@@ -1,17 +1,12 @@
 #pragma once
 
 #include <string>
+#include <optional>
 
 namespace Helper
 {
     struct EnumElement;
 
-    [[nodiscard]] std::string testEnumDatabase();
-    [[nodiscard]] std::string enumElement();
-
-    [[nodiscard]] std::string testEnumDatabaseOptionalMissing();
-    [[nodiscard]] std::string enumElementOptionalMissing();
-
-    [[nodiscard]] EnumElement loadEnumElement(std::string_view fileName);
+    [[nodiscard]] std::optional<EnumElement> loadEnumElement(std::string_view fileName);
     [[nodiscard]] int saveEnumElement(const EnumElement& element, std::string_view fileName);
 }
