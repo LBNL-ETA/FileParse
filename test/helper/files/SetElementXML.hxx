9fc9d89dbd64c39cf1b7bd6d6c1e5c210bd7cd66
Simon Vidanovic <dvvidanovic@lbl.gov>
2024-09-23 09:05:33 -0700

    All unit tests for sets are moved to files.

--- test/helper/files/SetElementXML.hxx
+++ test/helper/files/SetElementXML.hxx
@@ -9,23 +9,15 @@ namespace Helper
     struct SetElementOptionalDouble;
     struct SetElementEnum;
 
-    [[nodiscard]] std::string testSetElementEmptyDatabase();
-    [[nodiscard]] std::string setElementEmpty();
-
-    [[nodiscard]] std::string testSetElementOptionalDoubleDatabase();
-    [[nodiscard]] std::string setElementOptionalDouble();
-
-    [[nodiscard]] std::string testSetElementEnumDatabase();
-    [[nodiscard]] std::string setElementEnum();
-
     [[nodiscard]] std::optional<SetElementDouble> loadSetElementDouble(std::string_view fileName);
     [[nodiscard]] int saveSetElementDouble(const SetElementDouble & element,
                                            std::string_view fileName);
 
-    [[nodiscard]] SetElementOptionalDouble loadSetElementOptionalDouble(std::string_view fileName);
+    [[nodiscard]] std::optional<SetElementOptionalDouble>
+      loadSetElementOptionalDouble(std::string_view fileName);
     [[nodiscard]] int saveSetElementOptionalDouble(const SetElementOptionalDouble & element,
-                                      std::string_view fileName);
+                                                   std::string_view fileName);
 
-    [[nodiscard]] SetElementEnum loadSetElementEnum(std::string_view fileName);
+    [[nodiscard]] std::optional<SetElementEnum> loadSetElementEnum(std::string_view fileName);
     [[nodiscard]] int saveSetElementEnum(const SetElementEnum & element, std::string_view fileName);
 }   // namespace Helper
